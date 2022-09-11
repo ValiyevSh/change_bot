07775ee5ffe5ee9188aeba67daace38500d5e34a
shuxratbek <shuhratbekvaliyev3771@gmail.com>
2022-09-11 16:10:19 +0500

    new add

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
-echo "salom";
+include "Telegram.php";
+$telegram = new Telegram('5736961835:AAGa0FtSb9ov9nO4-kRmLJ8lOtCZMvtZMxA');
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$telegram->sendMessage($content);
